0b6738b8a888b9d20786207c61f6ec164236ce97
JPab <pabl0vargaas@hotmail.com>
2026-01-15 19:46:40 +0100

    Proyecto Base API

---
+++ src/main/java/com/supermercados/supermercados_api/SupermercadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.supermercados.supermercados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermercados/supermercados_api/SupermercadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermercados.supermercados_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
