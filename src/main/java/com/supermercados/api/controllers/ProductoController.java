995a93b3a8621629e8c1d2dc35229f8024a14c01
Yalin Sonia Luo <sonialuo10@gmail.com>
2026-01-18 18:40:53 +0100

    Creado ProductoController y SucursalController Revisar VentaController

--- src/main/java/com/supermercados/api/controllers/ProductoController.java
+++ src/main/java/com/supermercados/api/controllers/ProductoController.java
@@ -1,4 +1,55 @@
 package com.supermercados.api.controllers;
 
+import com.supermercados.api.dtos.requests.ProductoRequestDTO;
+import com.supermercados.api.dtos.responses.ProductoResponseDTO;
+import com.supermercados.api.models.ApiResponse;
+import com.supermercados.api.models.Producto;
+import com.supermercados.api.services.ProductoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+@RequiredArgsConstructor
 public class ProductoController {
+
+    private final ProductoService productoService;
+
+    //Muestra los productos existentes
+    @GetMapping
+    public ResponseEntity<List<Producto>> listar(){
+        return ResponseEntity.ok(productoService.listar());
+    }
+
+    //Permite crear un producto con los parametros establecidos
+    @PostMapping
+    public ResponseEntity<Producto> crear(@Valid @RequestBody Producto producto){
+        Producto productoCreado = productoService.crear(null, producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoCreado);
+    }
+
+    //Permite obtener el producto por ID
+    @PostMapping("{/id}")
+    public ResponseEntity<Producto> obtenerPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(productoService.obtenerPorId(id));
+    }
+
+    //Actualiza el producto
+    @PutMapping("{/id}")
+    public ResponseEntity<Producto> actualizar(@PathVariable Long id, @Valid @RequestBody Producto producto){
+        return ResponseEntity.status(HttpStatus.OK).body(productoService.actualizar(id, producto));
+    }
+
+    //Elimina un producto
+    @DeleteMapping
+    public ResponseEntity<Producto> eliminar(@PathVariable Long id){
+        productoService.eliminar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(productoService.obtenerPorId(id));
+    }
+
 }
--- src/main/java/com/supermercados/api/controllers/VentaController.java
+++ src/main/java/com/supermercados/api/controllers/VentaController.java
@@ -1,4 +1,45 @@
 package com.supermercados.api.controllers;
 
+import com.supermercados.api.dtos.requests.VentaRequestDTO;
+import com.supermercados.api.dtos.responses.VentaResponseDTO;
+import com.supermercados.api.services.VentaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ventas")
+@RequiredArgsConstructor
+
 public class VentaController {
-}
+
+    /* private final VentaService ventaService;
+
+    @PostMapping
+    public ResponseEntity<VentaResponseDTO> crearVenta(@Valid @RequestBody VentaRequestDTO dto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ventaService.crear(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VentaResponseDTO>> obtenerVentas(
+            @RequestParam(required = false)  Long id,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDate fecha) {
+
+        return ResponseEntity.ok(
+                ventaService.findBySucursalAndFecha(sucursalId, fecha));
+        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<VentaResponseDTO> eliminarVenta(@PathVariable Long id){
+        ventaService.deleteVenta(id);
+        return ResponseEntity.ok().body();
+    } */
+    }
