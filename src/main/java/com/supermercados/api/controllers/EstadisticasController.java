8fb185ee46a56c538fa93912fe881d24651d2eda
Fernando <feripa.6@gmail.com>
2026-01-22 21:10:47 +0100

    añadido Estadisticas controller y estadisticas service

--- src/main/java/com/supermercados/api/controllers/EstadisticasController.java
+++ src/main/java/com/supermercados/api/controllers/EstadisticasController.java
@@ -1,4 +1,35 @@
 package com.supermercados.api.controllers;
 
+
+import com.supermercados.api.dtos.producto.ProductoResponseDTO;
+import com.supermercados.api.models.Producto;
+import com.supermercados.api.services.EstadisticasService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/estadisticas")
+@RequiredArgsConstructor
 public class EstadisticasController {
+    private final EstadisticasService estadisticaService;
+
+    @GetMapping("/producto-mas-vendido")
+    public ResponseEntity<ProductoResponseDTO> productoMasVendido() {
+
+        Producto producto = estadisticaService.productoMasVendido();
+
+        ProductoResponseDTO dto = new ProductoResponseDTO(
+                producto.getId(),
+                producto.getNombre(),
+                producto.getPrecio(),
+                producto.getCategoria(),
+                producto.getStock()
+        );
+
+        return ResponseEntity.ok(dto);
+    }
+
 }
--- src/main/java/com/supermercados/api/services/EstadisticasService.java
+++ src/main/java/com/supermercados/api/services/EstadisticasService.java
@@ -1,4 +1,35 @@
 package com.supermercados.api.services;
 
+import com.supermercados.api.exceptions.ProductoNotFoundException;
+import com.supermercados.api.models.Producto;
+import com.supermercados.api.repositories.ProductoRepository;
+import com.supermercados.api.repositories.VentaProductoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class EstadisticasService {
+    private final VentaProductoRepository ventaProductoRepository;
+    private final ProductoRepository productoRepository;
+
+
+    public Producto productoMasVendido() {
+
+        List<Object[]> resultado = ventaProductoRepository.findProductosMasVendidos();
+
+        if (resultado.isEmpty()) {
+            throw new RuntimeException("No existen ventas registradas");
+        }
+
+        Long productoId = (Long) resultado.get(0)[0];
+
+        return productoRepository.findById(productoId)
+                .orElseThrow(() ->
+                        new ProductoNotFoundException("Producto no encontrado con id " + productoId));
+    }
+
 }
