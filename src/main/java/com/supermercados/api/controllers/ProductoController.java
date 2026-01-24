afcfe13674ff59714db4a73b2003d791f69fc38e
Yalin Sonia Luo <sonialuo10@gmail.com>
2026-01-24 20:51:24 +0100

    Se ha creado reestock de productos, ñistado de productos tanto activos como inactivos, reactivado de un producto eliminado

--- src/main/java/com/supermercados/api/controllers/ProductoController.java
+++ src/main/java/com/supermercados/api/controllers/ProductoController.java
@@ -1,17 +1,19 @@
 package com.supermercados.api.controllers;
 
-import com.supermercados.api.dtos.producto.ProductoMapper;
-import com.supermercados.api.dtos.producto.ProductoRequestDTO;
-import com.supermercados.api.dtos.producto.ProductoResponseDTO;
+import com.supermercados.api.dtos.producto.*;
 import com.supermercados.api.models.ApiResponse;
 import com.supermercados.api.models.Producto;
 import com.supermercados.api.services.ProductoService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.Priority;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,7 +23,7 @@ public class ProductoController {
 
     private final ProductoService productoService;
 
-    //Muestra los productos existentes
+    //Muestra los productos existentes (solo productos activos)
     @GetMapping
     public ResponseEntity<ApiResponse<List<ProductoResponseDTO>>> listar() {
         List<ProductoResponseDTO> productos = productoService.listar()
@@ -31,7 +33,7 @@ public class ProductoController {
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ApiResponse<>(true, "Lista de productos", productos));
+                .body(new ApiResponse<>(true, "Lista de productos (activos)", productos));
     }
 
     //Permite crear un producto con los parametros establecidos
@@ -58,5 +60,39 @@ public class ProductoController {
     public ResponseEntity<ApiResponse<Void>> eliminar(@PathVariable Long id){
         productoService.eliminar(id);
         return ResponseEntity.ok(new ApiResponse<>(true,"Producto eliminado correctamente", null));
+    }
+
+    //Reestock de un producto (solo para admin)
+    @PatchMapping("/{id}/stock")
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+
+    public ResponseEntity<ApiResponse<ProductoResponseDTO>> reponerStock(@PathVariable Long id, @Valid @RequestBody StockUpdateRequestDTO dto){
+        Producto actualizarStock = productoService.reponerStock(id, dto.getCantidad());
+        return ResponseEntity.ok(new ApiResponse<>(true, "Stock repuesto correctamente", ProductoMapper.toDTO(actualizarStock)));
+    }
+
+    //Listado de productos tanto activos como inactivos (solo para admin)
+    @GetMapping("/todos-los-productos")
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+
+    public ResponseEntity<ApiResponse<List<ProductoAdminResponseDTO>>> listarAdmin(){
+        List<ProductoAdminResponseDTO> allProducts = productoService.listarTodos()
+                .stream()
+                .map(ProductoMapper::toAdminDTO)
+                .toList();
+        return ResponseEntity.ok(new ApiResponse<>(true, "Lista completa de productos", allProducts));
+    }
+
+    //Reactiva un producto eliminado
+    @PatchMapping("/{id}/reactivar")
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<ApiResponse<ProductoResponseDTO>> reactivarProducto(@PathVariable Long id) {
+        Producto reactivarProducto = productoService.reactivar(id);
+        return ResponseEntity.ok(
+                new ApiResponse<>(true, "Producto reactivado correctamente", ProductoMapper.toDTO(reactivarProducto))
+        );
     }
 }
